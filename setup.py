66f939e4fe589aa63bf287cc66c1de1961dd4c66
Jesse Yen <jesse@domaintools.com>
2014-09-17 14:36:53 -0700

    added a few more thing in the setup.py meta section

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(name='webpyutils',
     description='web.py utilities to build quick RESTful API Services',
     author='Jesse V. Yen',
     author_email='jesse@jads.com',
-    url='https://github.com/jvy1106/webpyutils/tarball/0.1',
+    url='https://github.com/jvy1106/webpyutils',
+    download_url='https://github.com/jvy1106/webpyutils/tarball/0.1',
+    keywords = ['api', 'restful', 'server']
     install_requires=[
         'DBUtils',
         'web.py',
