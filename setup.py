26e5de7bd689e7b024a4d927af42a1125aa3c213
Jesse Yen <jesse@domaintools.com>
2014-09-17 13:57:27 -0700

    add required package files for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='webpyutils',
-    version='0.0.1',
+    version='0.1',
     description='web.py utilities to build quick RESTful API Services',
-    author='Jesse Yen',
+    author='Jesse V. Yen',
     author_email='jesse@jads.com',
-    url='',
+    url='https://github.com/jvy1106/webpyutils',
     install_requires=[
         'DBUtils',
         'web.py',
